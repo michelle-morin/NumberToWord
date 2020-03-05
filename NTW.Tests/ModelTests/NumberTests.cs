087fbe79c587afef253cc227ae24b1459fedebe0
Michelle Morin <morin2m@gmail.com>
2020-03-05 09:46:21 -0800

    add initial file structure to each project

---
+++ NTW.Tests/ModelTests/NumberTests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using NTW.Models;
+
+namespace NTW.Tests
+{
+  [TestClass]
+  public class NumberTests
+  {
+    [TestMethod]
+    public void ConvertNumber_RejectNonNumericalInputs_Error()
+    {
+      
+    }
+  }
+}
