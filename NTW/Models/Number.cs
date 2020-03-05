d2bbba232c441096d0d1d02e4b62b82655f4d29a
Michelle Morin <morin2m@gmail.com>
2020-03-05 12:54:32 -0800

    add logic to pass thousands place test

--- NTW/Models/Number.cs
+++ NTW/Models/Number.cs
@@ -5,28 +5,59 @@ namespace NTW.Models
 {
   public class Number
   {
-    public static Dictionary<char, string> ones { get; } = new Dictionary<char, string>() { {'0', "zero"}, {'1', "one"}, {'2', "two"}, {'3', "three"}, {'4', "four"}, {'5', "five"}, {'6', "six"}, {'7', "seven"}, {'8', "eight"}, {'9', "nine"} };
-    public static Dictionary<string, string> teens { get; } = new Dictionary<string, string>() { {"10", "ten"}, {"11", "eleven"}, {"12", "twelve"}, {"13", "thirteen"}, {"14", "fourteen"}, {"15", "fifteen"}, {"16", "sixteen"}, {"17", "seventeen"}, {"18", "eighteen"}, {"19", "nineteen"} };
-    public static Dictionary<char, string> tens { get; } = new Dictionary<char, string>() { {'2', "twenty"}, {'3', "thirty"}, {'4', "forty"}, {'5', "fifty"}, {'6', "sixty"}, {'7', "seventy"}, {'8', "eighty"}, {'9', "ninety"} };
+    public static Dictionary<char, string> ones { get; } = new Dictionary<char, string>() { 
+      {'0', "zero"}, 
+      {'1', "one"}, 
+      {'2', "two"}, 
+      {'3', "three"}, 
+      {'4', "four"}, 
+      {'5', "five"}, 
+      {'6', "six"}, 
+      {'7', "seven"}, 
+      {'8', "eight"}, 
+      {'9', "nine"} 
+    };
+    public static Dictionary<string, string> teens { get; } = new Dictionary<string, string>() {
+      {"10", "ten"}, 
+      {"11", "eleven"}, 
+      {"12", "twelve"}, 
+      {"13", "thirteen"}, 
+      {"14", "fourteen"}, 
+      {"15", "fifteen"}, 
+      {"16", "sixteen"}, 
+      {"17", "seventeen"}, 
+      {"18", "eighteen"}, 
+      {"19", "nineteen"} 
+    };
+    public static Dictionary<char, string> tens { get; } = new Dictionary<char, string>() { 
+      {'2', "twenty"}, 
+      {'3', "thirty"}, 
+      {'4', "forty"}, 
+      {'5', "fifty"}, 
+      {'6', "sixty"}, 
+      {'7', "seventy"}, 
+      {'8', "eighty"}, 
+      {'9', "ninety"} 
+    };
 
     public static string ConvertNumber(string stringInputNumber)
     {
+      char[] numberArray = stringInputNumber.ToCharArray();
       try
       {
         int number = int.Parse(stringInputNumber);
-        char[] numberArray = stringInputNumber.ToCharArray();
         string convertedNumber = "";
         if (stringInputNumber.Length <= 2)
         {
           convertedNumber = ConvertTwoDigits(stringInputNumber);
         }
-        else if (stringInputNumber.Length == 3 && numberArray[0] != '0')
+        else if (stringInputNumber.Length == 3)
         {
           convertedNumber = Number.ConvertThreeDigits(stringInputNumber, numberArray);
         }
-        else
+        else if (stringInputNumber.Length == 4)
         {
-          return "other";
+          convertedNumber = Number.ConvertFourDigits(stringInputNumber, numberArray);
         }
         return convertedNumber; 
       }
@@ -40,15 +71,48 @@ namespace NTW.Models
       }
     }
 
+    public static string ConvertFourDigits(string stringInputNumber, char[] numberArray)
+    {
+      char firstDigit = numberArray[0];
+      char[] otherDigits = { numberArray[1], numberArray[2], numberArray[3] };
+      string remainingDigits = new String(otherDigits);
+      string wordRemainder = Number.ConvertThreeDigits(remainingDigits, otherDigits);
+      if (numberArray[0] != '0')
+      {
+        string thousandsPlace = ones[firstDigit];
+        string wordNum = thousandsPlace + " thousand " + wordRemainder;
+        return wordNum;
+      }
+      else if (numberArray[0] == '0')
+      {
+        return wordRemainder;
+      }
+      else
+      {
+        return null;
+      }
+    }
+
     public static string ConvertThreeDigits(string stringInputNumber, char[] numberArray)
     {
       char firstDigit = numberArray[0];
-      string hundredsPlace = ones[firstDigit];
       char[] otherDigits = { numberArray[1], numberArray[2] };
       string remainingDigits = new String(otherDigits);
       string wordRemainder = Number.ConvertTwoDigits(remainingDigits);
-      string wordNum = hundredsPlace + " hundred " + wordRemainder;
-      return wordNum;
+      if (numberArray[0] != '0')
+      {
+        string hundredsPlace = ones[firstDigit];
+        string wordNum = hundredsPlace + " hundred " + wordRemainder;
+        return wordNum;
+      }
+      else if (numberArray[0] == '0')
+      {
+        return wordRemainder;
+      }
+      else
+      {
+        return null;
+      }
     }
 
     public static string ConvertTwoDigits(string stringInputNumber)
